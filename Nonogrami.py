8bae11b40257273ef7265b095f42b0f978cafd3d
Urša Zabukovec <39094176+ursazabukovec@users.noreply.github.com>
2018-05-15 22:34:37 +0200

    Dodala naslov okna in spremenila import

--- Nonogrami.py
+++ Nonogrami.py
@@ -1,7 +1,8 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 def navodila():
-    navodila = tk.Label(okno, text='Pravila so enostavna. Imate mrežo kvadratov, kateri morajo biti \
+    navodila = ttk.Label(okno, text='Pravila so enostavna. Imate mrežo kvadratov, kateri morajo biti \
 \n počrnjeni. Poleg vsake vrstice na mreži so naštete dolžine nizov črnih \
 \n kvadratov v tej vrstici. Nad vsakim stolpcem so naštete dolžine nizov črnih \
 \n kvadratov v tem stolpcu. Vaš cij je najti vse črne kvadrate. Kliknite z levo \
@@ -10,20 +11,21 @@ def navodila():
     navodila.grid(row=4, column=0)
     
 
-okno = tk.Tk()
-naslov = tk.Label(okno, text='Nonogrami')
+okno = Tk()
+okno.title('Nonogrami')
+naslov = ttk.Label(okno, text='Nonogrami')
 naslov.grid(row=0, column=0)
 
-nonogram1 = tk.Button(okno, text='Nonogram1')
+nonogram1 = ttk.Button(okno, text='Nonogram1')
 nonogram1.grid(row=1, column=1)
-nonogram2 = tk.Button(okno, text='Nonogram2')
+nonogram2 = ttk.Button(okno, text='Nonogram2')
 nonogram2.grid(row=1, column=2)
-nonogram3 = tk.Button(okno, text='Nonogram3')
+nonogram3 = ttk.Button(okno, text='Nonogram3')
 nonogram3.grid(row=2, column=1)
-nonogram4 = tk.Button(okno, text='Nonogram4')
+nonogram4 = ttk.Button(okno, text='Nonogram4')
 nonogram4.grid(row=2, column=2)
 
-gumb_navodila = tk.Button(okno, text='Navodila', command=navodila)
+gumb_navodila = ttk.Button(okno, text='Navodila', command=navodila)
 gumb_navodila.grid(row=3, column=1)
 
 okno.mainloop()
